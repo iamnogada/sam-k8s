3bc60f0a93008f0538b6982fc40ab8b65bd177a1
nogada <seungil.han@gmai.com>
2021-04-18 21:37:02 +0900

    clean

--- shell/app.sh
+++ shell/app.sh
@@ -2,5 +2,5 @@ docker run -d \
   --name app \
   -e SPRING_DATASOURCE_URL=jdbc:mariadb://mariadb:3306/employees \
   -p 8080:8080 \
-  --link mariadb
+  --link mariadb \
   docker.io/nogada/employees:v1 
